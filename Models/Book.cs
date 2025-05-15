87cd752b75b8d89f8caae3663a6fb20b6a183087
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-05-15 19:47:10 -0300

    feat: [Book] - Create a model for each book

---
+++ Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Library_Management.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum BookGender
+    {
+        Fiction,
+        Romance,
+        Action,
+        Terror
+    }
+
+    public class Book
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public BookGender Gender { get; set; }
+    }
+}

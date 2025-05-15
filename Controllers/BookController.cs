228f2edcdbfd419e3bc998397b7d496c00c1c367
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-05-15 19:49:07 -0300

    feat: [BookController] - Create controller and the [HttpPost] request

---
+++ Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using Library_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            return Created(string.Empty, book);
+        }
+
+    }
+}

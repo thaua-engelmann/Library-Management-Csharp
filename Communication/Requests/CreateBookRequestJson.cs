eb941d27fd89986cb4293f64494af51b09e316c8
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-05-17 01:00:48 -0300

    ✨ feat: [Communication] - Create folder to organize Requests and Responses

---
+++ Communication/Requests/CreateBookRequestJson.cs
@@ -0,0 +1,12 @@
+﻿using Library_Management.Models;
+
+namespace Library_Management.Communication.Requests
+{
+    public class CreateBookRequestJson
+    {
+
+        public string Name { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public BookGender Gender { get; set; }
+    }
+}
---
+++ Communication/Responses/CreatedBookResponseJson.cs
@@ -0,0 +1,14 @@
+﻿using Library_Management.Models;
+
+namespace Library_Management.Communication.Responses
+{
+    public class CreatedBookResponseJson
+    {
+
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+
+        public BookGender Gender {  get; set; }
+    }
+}

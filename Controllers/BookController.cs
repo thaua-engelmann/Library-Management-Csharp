111493e45e4bc483c58b707074a32a7f066c6d87
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-05-17 01:01:45 -0300

    fix: [BookController] - Adjust HttpPost to use both CreateBookRequestJson and CreatedBookResponseJson

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,4 +1,6 @@
-﻿using Library_Management.Models;
+﻿using Library_Management.Communication.Requests;
+using Library_Management.Communication.Responses;
+using Library_Management.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library_Management.Controllers
@@ -9,11 +11,18 @@ namespace Library_Management.Controllers
     {
 
         [HttpPost]
-        [ProducesResponseType(typeof(Book), StatusCodes.Status201Created)]
-        public IActionResult Create([FromBody] Book book)
+        [ProducesResponseType(typeof(CreatedBookResponseJson), StatusCodes.Status201Created)]
+        public IActionResult Create([FromBody] CreateBookRequestJson request)
         {
 
-            return Created(string.Empty, book);
+            var response = new CreatedBookResponseJson
+            {
+                Name = request.Name,
+                Author = request.Author,
+                Gender = request.Gender,
+            };
+
+            return Created(string.Empty, response);
         }
 
     }

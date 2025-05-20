9d146edf0c1f1f28e81d9fdce34d5e00667ae9c3
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-05-20 19:35:09 -0300

    ✨ feat: [BookService] - Create a singleton service + interface to persist data

---
+++ Api/Services/BookService.cs
@@ -0,0 +1,22 @@
+﻿using Library_Management.Api.Entities;
+using Library_Management.Api.Services.Interfaces;
+
+namespace Library_Management.Api.Services
+{
+    public class BookService : IBookService
+    {
+
+        private readonly List<Book> _books = new();
+
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+
+        public void Add(Book book)
+        {
+            _books.Add(book);
+        }
+
+    }
+}
---
+++ Api/Services/Interfaces/IBookService.cs
@@ -0,0 +1,10 @@
+﻿using Library_Management.Api.Entities;
+
+namespace Library_Management.Api.Services.Interfaces
+{
+    public interface IBookService
+    {
+        List<Book> GetAll();
+        void Add(Book book);
+    }
+}

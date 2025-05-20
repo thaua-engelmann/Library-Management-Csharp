a651760bd284358d8acaf5eeb4f7dc164f784beb
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-05-20 19:59:25 -0300

    🐛 fix: [CreateBookResponseJson] - Must not generate a new Guid

--- Communication/Responses/CreatedBookResponseJson.cs
+++ Communication/Responses/CreatedBookResponseJson.cs
@@ -5,7 +5,7 @@ namespace Library_Management.Communication.Responses
     public class CreatedBookResponseJson
     {
 
-        public Guid Id { get; } = Guid.NewGuid();
+        public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public decimal Price { get; set; }

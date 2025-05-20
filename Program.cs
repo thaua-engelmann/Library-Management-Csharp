307d91abf09578d8558752d7f807a35e50aabb33
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-05-20 19:03:36 -0300

    feat: [AutoMapper] - Add new package called AutoMapper@14.0.0 to map classes

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 using System.Text.Json.Serialization;
+using AutoMapper;
+using Library_Management.Api.Entities;
+using Library_Management.Communication.Responses;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-}); ;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,7 +17,16 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
+// Configure AutoMapper.
+builder.Services.AddAutoMapper(typeof(Program));
+
 var app = builder.Build();
+
+// Configure AutoMapper package.
+//var config = new MapperConfiguration(cfg =>
+//{
+//    cfg.CreateMap<Book, CreatedBookResponseJson>();
+//});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
